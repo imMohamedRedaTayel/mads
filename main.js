eeadfab53562fcacc8706c3247e1f209fa060cbd
imMohamedRedaTayel <mohamedreda65775@gmail.com>
2024-12-28 18:05:36 +0200

    save

--- main.js
+++ main.js
@@ -21,3 +21,11 @@ scrollIconContainer.addEventListener('click', () => {
 });
 
 
+document.addEventListener('DOMContentLoaded', () => {
+    AOS.init({
+      duration: 1000, 
+      offset: 200,
+      easing: 'ease-in-out', 
+      once: true, 
+    });
+  });
